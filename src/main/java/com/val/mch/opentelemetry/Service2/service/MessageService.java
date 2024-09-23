5c7c94d3ab659d0de6185ea8ba03170522fe076f
maxime_chauvel <chauvel.maxime@valeuriad.fr>
2024-09-23 17:11:11 +0200

    [WIP] Apero OTEL

--- src/main/java/com/val/mch/opentelemetry/Service2/service/MessageService.java
+++ src/main/java/com/val/mch/opentelemetry/Service2/service/MessageService.java
@@ -4,6 +4,7 @@ import com.val.mch.opentelemetry.Service2.config.messaging.BrokerConfiguration;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class MessageService {
     private final RabbitTemplate rabbitTemplate;
@@ -15,6 +16,7 @@ public class MessageService {
     public String produceMessage(String message) {
         rabbitTemplate.convertAndSend(BrokerConfiguration.EXCHANGE_NAME, "myRoutingKey.messages",
                 message);
+
         return "Message(" + message + ")" + " has been produced.";
     }
 }
